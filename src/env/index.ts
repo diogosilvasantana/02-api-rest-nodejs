b00c08abece9d61c246571755dd4ec3e97dba915
Proart Studio <proartstudio@iMac-Pro-de-Proart.local>
2023-07-20 21:55:37 -0300

    chore: Production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -7,6 +7,7 @@ process.env.NODE_ENV === "test"
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 });
